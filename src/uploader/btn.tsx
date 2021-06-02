c374201e352fd67c7d727ce57b4b6c1cc050dedd
lirongtong <lirongtong@hotmail.com>
2021-06-02 16:36:13 +0800

    单图 - input 上传按钮

--- src/uploader/btn.tsx
+++ src/uploader/btn.tsx
@@ -7,7 +7,19 @@ export default defineComponent({
     inheritAttrs: false,
     props: {...UploaderProps},
     setup(props, { slots }) {
+        // class name prefix
         const prefixCls = props.prefixCls ?? 'mi-uploader'
+        // accepts
+        const accepts = {
+            image: ['image/*'],
+            doc: [
+                '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
+                '.pdf', '.txt', '.zip', '.rar', '.7z', '.tif',
+                '.tiff', 'csv', 'epub', 'pages', 'numbers', 'keynote'
+            ],
+            audio: ['audio/*'],
+            video: ['video/*']
+        }
         const images = {
             single: {
                 cls: `${prefixCls}-image`,
@@ -17,12 +29,18 @@ export default defineComponent({
             },
             multiple: {}
         }
+        // single image
         images.single.elem = <div class={images.single.cls} style={images.single.style}>
             <div class={`${images.single.cls}-icon`}>
                 <FileImageOutlined />上传图片
             </div>
             <div class={`${images.single.cls}-preview`}></div>
             <div class={`${images.single.cls}-progress`}></div>
+            <div class={`${images.single.cls}-input`}>
+                <input type="file"
+                    multiple={props.multiple}
+                    accept={props.accept ?? accepts.image.join(',')} />
+            </div>
         </div>
         let elem = null
         if (props.type === 'image' && !props.multiple) elem = images.single.elem
--- src/uploader/uploader.tsx
+++ src/uploader/uploader.tsx
@@ -8,16 +8,6 @@ const MiUploader =  defineComponent({
     props: { ...UploaderProps },
     setup(props) {
         const prefixCls = props.prefixCls ?? 'mi-uploader'
-        const accepts = {
-            image: ['image/*'],
-            document: [
-                '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
-                '.pdf', '.txt', '.zip', '.rar', '.7z', '.tif',
-                '.tiff', 'csv', 'epub', 'pages', 'numbers', 'keynote'
-            ],
-            audio: ['audio/*'],
-            video: ['video/*']
-        }
         return () => <div class={`${prefixCls}-container`}>
             <MiUploaderBtn {...props} />
         </div>
